05de3f5d13204e691a5b0ef42f06f9fb2d12c2e2
Lucas <lucas.sodre.teixeira@outlook.com>
2019-12-26 15:43:04 -0300

    versão 2.0.0 do app

---
+++ simple-note-js/src/components/App.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import uuid from 'uuid/v1'
+import AppBar from './AppBar'
+import NewNote from './NewNote'
+import NoteList from './NoteList'
+
+class App extends React.Component {
+	state = {
+		notes: []
+	}
+
+	handleAddNote = text => {
+		this.setState(prevState => ({
+			notes: prevState.notes.concat({
+				id: uuid(),
+				text
+			})
+		}))
+	}
+
+	handleMove = (direction, index) => {
+		this.setState(prevState => {
+			const newNotes = prevState.notes.slice()
+			const removedNote = newNotes.splice(index, 1)[0]
+
+			if (direction === 'up') {
+				newNotes.splice(index - 1, 0, removedNote)
+			} else if (direction === 'down') {
+				newNotes.splice(index + 1, 0, removedNote)
+			}
+
+			return {
+				notes: newNotes
+			}
+		})
+	}
+
+	handleDelete = note => {
+		let confirmDelete = confirm(
+			`Você deseja remover a nota "${note.text}"?`
+		)
+		if (confirmDelete) {
+			this.setState(prevState => {
+				const newNotes = prevState.notes.slice()
+				const index = newNotes.findIndex(note => note.id === note.id)
+				newNotes.splice(index, 1)[0]
+
+				return {
+					notes: newNotes
+				}
+			})
+		}
+	}
+
+	handleEdit = (id, text) => {
+		this.setState(prevState => {
+			const newNotes = prevState.notes.slice()
+			const index = newNotes.findIndex(note => note.id === id)
+			newNotes[index].text = text
+
+			return {
+				notes: newNotes
+			}
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<AppBar />
+				<div className='container'>
+					<NewNote onAddNote={this.handleAddNote} />
+					<NoteList
+						notes={this.state.notes}
+						onMove={this.handleMove}
+						onDelete={this.handleDelete}
+						onEdit={this.handleEdit}
+					/>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default App
---
+++ simple-note-js/src/components/AppBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppBar = () => (
+	<div className={'app-bar'}>
+		<div className={'app-bar__container'}>simple note.js</div>
+	</div>
+)
+
+export default AppBar
